d8a1b319071d7632bec097043f58d5c14cdb5ef4
Alex <alexoh@wcoding.com>
2021-12-02 10:24:43 +0900

    Up to 16.

---
+++ 14.2.php
@@ -0,0 +1,60 @@
+<?php
+
+// $allStringLines = [
+//     'Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.',
+//     'Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.',
+// ];
+$allStringLines = explode("\n", file_get_contents('13.in.txt'));
+
+$reindeer = [];
+
+foreach ($allStringLines as $line) {
+    $line_tokens = explode(' ', $line);
+    $reindeer[$line_tokens[0]] = [
+        'speed' => $line_tokens[3],
+        'fly' => $line_tokens[6],
+        'period' => $line_tokens[6] + $line_tokens[13],
+        'points' => 0,
+        ];
+}
+
+function at_seconds($in_time)
+{
+    global $reindeer;
+
+    $farthest_distance = 0;
+    $farthest_reindeer = '';
+
+    foreach ($reindeer as $name => $val) {
+        $integral = intval($in_time / $val['period']);
+        $fractional = $in_time % $val['period'];
+
+        if ($fractional > $val['fly']) {
+            $fractional = $val['fly'];
+        }
+
+        $distance = ($integral * $val['fly'] * $val['speed']) + ($fractional * $val['speed']);
+
+        if ($distance > $farthest_distance) {
+            $farthest_distance = $distance;
+            $farthest_reindeer = $name;
+        }
+    }
+
+    return $farthest_reindeer;
+}
+
+$points_leader = '';
+$points_leader_has = 0;
+foreach (range(1, 2503) as $sec) {
+    // foreach (range(1, 1000) as $sec) {
+    $winner = at_seconds($sec);
+    ++$reindeer[$winner]['points'];
+
+    if ($reindeer[$winner]['points'] > $points_leader_has) {
+        $points_leader_has = $reindeer[$winner]['points'];
+        $points_leader = $winner;
+    }
+}
+
+echo "POINTS: ${points_leader_has} ${points_leader}".PHP_EOL;
